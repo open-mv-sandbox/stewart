36160572798cf673e17c0b02da790e023a2afa96
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2023-08-05 12:41:48 +0200

    Move most of HTTP example to HTTP crate

--- examples/html_hello.rs
+++ examples/html_hello.rs
@@ -4,9 +4,7 @@ use std::rc::Rc;
 
 use anyhow::Error;
 use stewart::{Actor, Context, World};
-use stewart_message::{mailbox, Mailbox, Sender};
-use stewart_mio::{net::tcp, Registry};
-use tracing::{event, Level};
+use stewart_mio::Registry;
 
 fn main() -> Result<(), Error> {
     utils::init_logging();
@@ -15,7 +13,7 @@ fn main() -> Result<(), Error> {
     let registry = Rc::new(Registry::new()?);
 
     // Start the actor
-    let actor = Service::new(&mut world, &registry)?;
+    let actor = Service::new(&mut world, registry.clone())?;
     world.insert("html-example", actor)?;
 
     // Run the event loop
@@ -24,115 +22,30 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-struct Service {
-    server_mailbox: Mailbox<tcp::ListenerEvent>,
-    _server_sender: Sender<tcp::ListenerAction>,
-
-    connections: Vec<Connection>,
-}
-
-struct Connection {
-    event: tcp::ConnectedEvent,
-    pending: String,
-    closed: bool,
-}
+struct Service {}
 
 impl Service {
-    pub fn new(world: &mut World, registry: &Rc<Registry>) -> Result<Self, Error> {
-        let (server_mailbox, on_event) = mailbox();
+    pub fn new(world: &mut World, registry: Rc<Registry>) -> Result<Self, Error> {
+        stewart_http::listen(
+            world,
+            registry,
+            "127.0.0.1:1234".parse()?,
+            RESPONSE.to_string(),
+        )?;
 
-        // Start the listen port
-        let (server_sender, server_info) =
-            tcp::listen(world, registry.clone(), "127.0.0.1:1234".parse()?, on_event)?;
-        event!(Level::INFO, addr = ?server_info.local_addr, "listening");
-
-        let actor = Service {
-            server_mailbox,
-            _server_sender: server_sender,
-
-            connections: Vec::new(),
-        };
+        let actor = Service {};
         Ok(actor)
     }
 }
 
 impl Actor for Service {
-    fn register(&mut self, ctx: &mut Context) -> Result<(), Error> {
-        self.server_mailbox.set_signal(ctx.signal());
+    fn register(&mut self, _ctx: &mut Context) -> Result<(), Error> {
         Ok(())
     }
 
-    fn process(&mut self, ctx: &mut Context) -> Result<(), Error> {
-        self.poll_listener(ctx)?;
-        self.poll_connections()?;
-
+    fn process(&mut self, _ctx: &mut Context) -> Result<(), Error> {
         Ok(())
     }
 }
 
-impl Service {
-    fn poll_listener(&mut self, ctx: &mut Context) -> Result<(), Error> {
-        while let Some(event) = self.server_mailbox.recv() {
-            match event {
-                tcp::ListenerEvent::Connected(event) => {
-                    event!(Level::INFO, "stream accepted");
-
-                    // Keep track of the stream
-                    event.event_mailbox.set_signal(ctx.signal());
-                    let connection = Connection {
-                        event,
-                        pending: String::new(),
-                        closed: false,
-                    };
-                    self.connections.push(connection);
-                }
-                tcp::ListenerEvent::Closed => ctx.set_stop(),
-            }
-        }
-
-        Ok(())
-    }
-
-    fn poll_connections(&mut self) -> Result<(), Error> {
-        for connection in &mut self.connections {
-            while let Some(event) = connection.event.event_mailbox.recv() {
-                match event {
-                    tcp::StreamEvent::Recv(event) => {
-                        event!(Level::DEBUG, bytes = event.data.len(), "received data");
-
-                        let data = std::str::from_utf8(&event.data)?;
-                        connection.pending.push_str(data);
-                    }
-                    tcp::StreamEvent::Closed => {
-                        event!(Level::INFO, "stream closed");
-                        connection.closed = true;
-                    }
-                }
-            }
-
-            if !connection.closed {
-                // Check if we have a full request worth of data
-                let split = connection.pending.split_once("\r\n\r\n");
-                if let Some((_left, right)) = split {
-                    event!(Level::INFO, "responding to request");
-                    connection.pending = right.to_string();
-
-                    // Send the response
-                    let action = tcp::SendAction {
-                        data: RESPONSE.to_vec(),
-                    };
-                    connection
-                        .event
-                        .actions_sender
-                        .send(tcp::StreamAction::Send(action))?;
-                }
-            }
-        }
-
-        self.connections.retain(|c| !c.closed);
-
-        Ok(())
-    }
-}
-
-const RESPONSE: &[u8] = b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 63\r\n\r\n<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>";
+const RESPONSE: &str = "<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>";
